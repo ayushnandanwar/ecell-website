fc17f6d30172a664afb88296f10a0041f49d2b07
Ayush Nandanwar <ayush.nandanwar@mitaoe.ac.in>
2022-10-14 23:28:59 +0530

    some animation added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function reveal() {
+    var reveals = document.querySelectorAll("section");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 250;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("section_ani");
+      } else {
+        reveals[i].classList.remove("section_ani");
+      }
+      if(elementTop < -330) reveals[i].classList.remove("section_ani");
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  reveal()
